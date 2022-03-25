be3512aa42abc070a5911df243529d7ca26ba070
XXiaoA <isxxiaoa@gmail.com>
2022-03-25 18:39:50 +0800

    config indentBlankline

--- lua/plugin-config/indent_blankline.lua
+++ lua/plugin-config/indent_blankline.lua
@@ -1,25 +1,53 @@
-vim.opt.termguicolors = true
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+local o = vim.opt
+local g = vim.g
 
-vim.opt.list = true
--- vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+o.list = true
+-- o.listchars:append("space:⋅")
+o.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
-    -- char_highlight_list = {
-    --     "IndentBlanklineIndent1",
-    --     "IndentBlanklineIndent2",
-    --     "IndentBlanklineIndent3",
-    --     "IndentBlanklineIndent4",
-    --     "IndentBlanklineIndent5",
-    --     "IndentBlanklineIndent6",
-    -- },
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = true
 }
+
+g.indent_blankline_context_patterns = {
+    "class",
+    "function",
+    "method",
+    "^if",
+    "^while",
+    "^typedef",
+    "^for",
+    "^object",
+    "^table",
+    "block",
+    "arguments",
+    "typedef",
+    "while",
+    "^public",
+    "return",
+    "if_statement",
+    "else_clause",
+    "jsx_element",
+    "jsx_self_closing_element",
+    "try_statement",
+    "catch_clause",
+    "import_statement"
+}
+
+g.indent_blankline_filetype_exclude = {
+    "help",
+    "startify",
+    "TERMINAL",
+    "terminal",
+    "packer",
+    "lsp-installer",
+    "",
+    "startuptime",
+    "toggleterm",
+    "translator",
+    "dashboard"
+}
+g.indent_blankline_show_end_of_line = false -- 占用隐藏符号
+g.indent_blankline_show_trailing_blankline_indent = false -- 删除多余的缩进线
