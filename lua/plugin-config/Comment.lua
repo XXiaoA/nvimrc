1b32d5bbc258464f53df5115a0282e33a6c52c54
XXiaoA <isxxiaoa@gmail.com>
2022-03-25 18:38:17 +0800

    open extended option

--- lua/plugin-config/Comment.lua
+++ lua/plugin-config/Comment.lua
@@ -55,7 +55,7 @@ require'Comment'.setup{
         extra = true,
         ---Extended mapping
         ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
-        extended = false,
+        extended = true,
     },
 
     ---Pre-hook, called before commenting the line
