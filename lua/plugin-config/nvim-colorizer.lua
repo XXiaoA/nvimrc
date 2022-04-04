a14972f16de15d41e62bb5db5ed47b869572bd92
XXiaoA <isxxiaoa@gmail.com>
2022-04-04 11:20:29 +0800

    configure nvim-colorizer

--- lua/plugin-config/nvim-colorizer.lua
+++ lua/plugin-config/nvim-colorizer.lua
@@ -4,37 +4,6 @@ if not ok then
     return
 end
 
--- Attach to certain Filetypes, add special configuration for `html`
--- Use `background` for everything else.
 colorizer.setup {
-  'css';
-  'javascript';
-  html = {
-    mode = 'foreground';
-  }
-}
-
--- Use the `default_options` as the second parameter, which uses
--- `foreground` for every mode. This is the inverse of the previous
--- setup configuration.
-colorizer.setup({
-  'css';
-  'javascript';
-  html = { mode = 'background' };
-}, { mode = 'foreground' })
-
--- Use the `default_options` as the second parameter, which uses
--- `foreground` for every mode. This is the inverse of the previous
--- setup configuration.
-colorizer.setup {
-  '*'; -- Highlight all files, but customize some others.
-  css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
-  html = { names = false; } -- Disable parsing "names" like Blue or Gray
-}
-
--- Exclude some filetypes from highlighting by using `!`
-colorizer.setup {
-  '*'; -- Highlight all files, but customize some others.
-  '!vim'; -- Exclude vim from highlighting.
-  -- Exclusion Only makes sense if '*' is specified!
+    "*",
 }
