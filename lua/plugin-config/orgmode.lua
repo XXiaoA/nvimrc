11330dfb4e29db5b80b7f17b23679c6cf4a47cfe
XXiaoA <isxxiaoa@gmail.com>
2022-04-06 22:13:18 +0800

    save

--- lua/plugin-config/orgmode.lua
+++ lua/plugin-config/orgmode.lua
@@ -7,18 +7,6 @@ end
 -- Load custom tree-sitter grammar for org filetype
 orgmode.setup_ts_grammar()
 
--- Tree-sitter configuration
-local ok, ntc = pcall(require, "nvim-treesitter.configs")
-ntc.setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
-}
-
 orgmode.setup({
   org_agenda_files = {'~/org/*', '~/my-orgs/**/*'},
   org_default_notes_file = '~/org/refile.org',
