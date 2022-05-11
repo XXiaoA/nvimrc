5f02d09a207bfb04e9166b34ba22ec592859ed8c
XXiaoA <isxxiaoa@gmail.com>
2022-05-11 20:41:17 +0800

    key mapping: * # g* g#

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -99,10 +99,10 @@ map("n", "<leader>i", "gg=G")
 -- hlslens
 map('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]])
 map('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]])
-map('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]])
-map('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]])
-map('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]])
-map('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]])
+map('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>N]])
+map('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>N]])
+map('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>N]])
+map('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>N]])
 
 
 -- lsp 回调函数快捷键设置
