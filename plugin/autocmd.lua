784d166d29cbfe32217fbb9739a54ff099e80525
XXiaoA <isxxiaoa@gmail.com>
2022-05-11 21:30:28 +0800

    Disable inserting comment leader after hitting o or O or <Enter>

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,11 +1,12 @@
+local api = vim.api
 -- Exit nvim when we only have the following types of windows {{{
 local quit_current_win = function()
     local quit_filetypes = {"NvimTree", "aerial"}
     local should_quit = true
-    local tabwins = vim.api.nvim_tabpage_list_wins(0)
+    local tabwins = api.nvim_tabpage_list_wins(0)
     for _, w in ipairs(tabwins) do
-        local buf = vim.api.nvim_win_get_buf(w)
-        local bf = vim.api.nvim_buf_get_option(buf, "filetype")
+        local buf = api.nvim_win_get_buf(w)
+        local bf = api.nvim_buf_get_option(buf, "filetype")
         if not vim.tbl_contains(quit_filetypes, bf) then
             should_quit = false
         end
@@ -16,13 +17,13 @@ local quit_current_win = function()
     end
 end
 
-vim.api.nvim_create_autocmd("BufEnter", {callback = quit_current_win})
+api.nvim_create_autocmd("BufEnter", {callback = quit_current_win})
 -- }}}
 
 -- smart number from https://github.com/jeffkreeftmeijer/vim-numbertoggle {{{
-local numbertoggle = vim.api.nvim_create_augroup(("numbertoggle"), {clear = true})
+local numbertoggle = api.nvim_create_augroup(("numbertoggle"), {clear = true})
 
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
     {"BufEnter", "FocusGained", "InsertLeave", "WinEnter"},
     {
         command = [[if &nu && mode() != 'i' | set rnu   | endif]],
@@ -30,7 +31,7 @@ vim.api.nvim_create_autocmd(
     }
 )
 
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
     {"BufLeave", "FocusLost", "InsertEnter", "WinLeave"},
     {
         command = [[if &nu | set nornu | endif]],
@@ -39,10 +40,18 @@ vim.api.nvim_create_autocmd(
 )
 -- }}}
 
-vim.api.nvim_create_autocmd(
+api.nvim_create_autocmd(
     "BufWritePost",
     {
         pattern = "plugins.lua",
         command = "source <afile> | PackerCompile"
+    }
+)
+
+-- Disable inserting comment leader after hitting o or O or <Enter>
+api.nvim_create_autocmd(
+    {"BufRead", "BufNewFile"},
+    {
+        command = "set formatoptions-=ro"
     }
 )
