ef42041e38ec479be8ac64d47703b59a564775c7
XXiaoA <isxxiaoa@gmail.com>
2022-05-20 21:40:48 +0800

    utils: creat two new functions: change_colorscheme() and change_background()

---
+++ lua/utils/colorscheme.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.change_colorscheme = function(colorscheme)
+    -- the theme has lualine's colorscheme
+    local lualine_theme = {"nightfox", "gruvbox-material"}
+
+    if vim.tbl_contains(lualine_theme, colorscheme) then
+        local lualine = require("utils").requirePlugin("lualine")
+        lualine.setup {options = {theme = colorscheme}}
+    end
+    -- vim.g.colors_name = colorscheme
+    pcall(vim.cmd, "colorscheme " .. colorscheme)
+end
+
+M.change_background = function(background)
+    pcall(vim.cmd, "set background=" .. background)
+end
+
+return M
