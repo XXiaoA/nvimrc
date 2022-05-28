2c665aec6110c6a4dadbaab1f87b5c5ec2312bce
XXiaoA <isxxiaoa@gmail.com>
2022-05-28 19:39:37 +0800

    configure nvim-tree and nvim-treesitter

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 local nvim_tree = require("utils").requirePlugin("nvim-tree")
 
 if nvim_tree and nvim_tree ~= true then
-    vim.g.nvim_tree_respect_buf_cwd = 1
+    respect_buf_cwd = 1
 
     nvim_tree.setup {
         update_cwd = true,
--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ if tsc then
         -- 启用代码高亮功能
         highlight = {
             enable = true,
-            additional_vim_regex_highlighting = false
+            additional_vim_regex_highlighting = true
         },
         -- 彩色括号
         rainbow = {
