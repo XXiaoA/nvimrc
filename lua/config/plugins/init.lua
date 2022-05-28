0dce612271afd2ad36ec1fd930074df053bbc648
XXiaoA <isxxiaoa@gmail.com>
2022-05-28 18:15:00 +0800

    feat: throw the detailed messages when load plugin

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -4,10 +4,13 @@ local all_files =
 all_files = vim.api.nvim_eval(all_files)
 
 for _, file in pairs(all_files) do
-    local plugin = string.match(file, "lua/config/plugins/(.+).lua")
-    if plugin ~= "init" then
-        if not pcall(require, "config/plugins/" .. plugin) then
-            vim.notify(" Failed to load file: lua/config/plugins/" .. plugin .. ".lua", vim.log.levels.WARN)
+    local plugin_name = string.match(file, "lua/config/plugins/(.+).lua")
+
+    if plugin_name ~= "init" then
+        local status_ok, plugin = pcall(require, "config/plugins/" .. plugin_name)
+        if not status_ok then
+            vim.notify(" Failed to load file: lua/config/plugins/" .. plugin_name .. ".lua", vim.log.levels.WARN)
+            vim.notify(plugin, vim.log.levels.WARN)
         end
     end
 end
