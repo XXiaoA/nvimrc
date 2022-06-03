fc8d0f2761d49c46691e7e1faafb762bc025cd57
XXiaoA <isxxiaoa@gmail.com>
2022-06-03 09:57:49 +0800

    feat(formatter): use stylua instead of luafmt

--- lua/config/plugins/format.lua
+++ lua/config/plugins/format.lua
@@ -1,43 +1,46 @@
 local formatter = require("utils").requirePlugin("formatter")
 
 local lua_config = {
-    -- luafmt
     function()
         return {
-            exe = "luafmt",
-            args = {"--indent-count", 4, "--stdin"},
-            stdin = true
+            exe = "stylua",
+            args = {
+                "--indent-type Spaces",
+                "--",
+                "-",
+            },
+            stdin = true,
         }
-    end
+    end,
 }
 
 local python_config = {
     function()
         return {
             exe = "black",
-            args = {"-"},
-            stdin = true
+            args = { "-" },
+            stdin = true,
         }
-    end
-} 
+    end,
+}
 
 local cpp_config = {
     function()
         return {
             exe = "clang-format",
-            args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
+            args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
             stdin = true,
-            cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
+            cwd = vim.fn.expand("%:p:h"), -- Run clang-format in cwd of the file.
         }
-    end
+    end,
 }
 
 if formatter then
-    formatter.setup {
+    formatter.setup({
         filetype = {
             lua = lua_config,
             python = python_config,
-            cpp = cpp_config
-        }
-    }
+            cpp = cpp_config,
+        },
+    })
 end
