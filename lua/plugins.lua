95701e86dbd1ba9648084fdc08aee217fcb48fdd
XXiaoA <isxxiaoa@gmail.com>
2022-06-04 11:12:49 +0800

    feat(packer.nvim): change the install link

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,8 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://hub.fastgit.xyz/wbthomason/packer.nvim.git", install_path })
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    -- fn.system({ "git", "clone", "--depth", "1", "https://hub.fastgit.xyz/wbthomason/packer.nvim.git", install_path })
     vim.cmd("packadd packer.nvim")
 end
 
