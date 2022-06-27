779f544f2460050b5d6879e5a7af5fcb1d8817e3
XXiaoA <isxxiaoa@gmail.com>
2022-06-27 20:53:25 +0800

    utils(requirePlugin) more friendly hint

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -3,7 +3,7 @@ local M = {}
 M.requirePlugin = function(plugin_name)
     local status_ok, plugin = pcall(require, plugin_name)
     if not status_ok then
-        vim.notify(" Failed to load: " .. plugin_name, vim.log.levels.WARN)
+        vim.notify(" Failed to load: " .. plugin_name .. " (From requirePlugin)", vim.log.levels.WARN)
         return nil
     else
         if plugin ~= true then
