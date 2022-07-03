adea30340efe362af7fbae8fadeafa41164b3877
XXiaoA <isxxiaoa@gmail.com>
2022-07-03 21:53:45 +0800

    add cpp snippets

---
+++ snippets/cpp.lua
@@ -0,0 +1,20 @@
+return {
+    s( "main", fmt([[
+    int main() {
+        <>
+
+        return 0;
+    }
+    ]], i(1), {delimiters = "<>"})
+    ),
+
+    s( "#inc", fmt([[
+    #include "{}"
+    ]], i(1))
+    ),
+
+    s( "#inc<", fmt([[
+    #include <{}>
+    ]], i(1))
+    ),
+}
