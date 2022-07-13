67dc2d178f531f20f44a5f00b025f41a2d752005
XXiaoA <isxxiaoa@gmail.com>
2022-07-13 16:07:25 +0800

    feat(utils): requirePluginc can choose send message or not

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,9 +1,11 @@
 local M = {}
 
-M.requirePlugin = function(plugin_name)
+M.requirePlugin = function(plugin_name, message)
     local status_ok, plugin = pcall(require, plugin_name)
     if not status_ok then
-        vim.notify(" Failed to load: " .. plugin_name .. " (From requirePlugin)", vim.log.levels.WARN)
+        if message ~= false then
+            vim.notify(" Failed to load: " .. plugin_name .. " (From requirePlugin)", vim.log.levels.WARN)
+        end
         return nil
     else
         if plugin ~= true then
