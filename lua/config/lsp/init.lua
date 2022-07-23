3f61facc338c456665e838b2fb26b416dcc19712
XXiaoA <isxxiaoa@gmail.com>
2022-07-23 18:44:20 +0800

    feat(lsp): add keymaps

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -23,32 +23,28 @@ mason_lspconfig.setup_handlers({
     end,
 })
 
--- function maplsp(mapbuf)
---     local opt = {
---         noremap = true,
---         silent = true,
---     }
---     -- rename
---     mapbuf("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
---     -- code action
---     mapbuf("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opt)
+local nmap = require("core.keymap").set_keymap("n")
 
---     -- go xx
---     mapbuf("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
---     mapbuf("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
---     mapbuf("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
---     mapbuf("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
---     mapbuf("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
+-- rename
+nmap({ "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>" })
+-- code action
+nmap({ "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>" })
 
---     -- diagnostic
---     mapbuf("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
---     mapbuf("n", "gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
---     mapbuf("n", "gn", "<cmd>lua vim.diagnostic.goto_next()<CR>", opt)
---     -- mapbuf('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
---     mapbuf("n", "<gk>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opt)
---     mapbuf("n", "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opt)
---     -- mapbuf('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
---     -- mapbuf('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
---     -- mapbuf('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
---     -- mapbuf('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
--- end
+-- go xx
+nmap({ "gd", "<cmd>lua vim.lsp.buf.definition()<CR>" })
+nmap({ "gh", "<cmd>lua vim.lsp.buf.hover()<CR>" })
+nmap({ "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>" })
+nmap({ "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>" })
+nmap({ "gr", "<cmd>lua vim.lsp.buf.references()<CR>" })
+
+-- diagnostic
+nmap({ "go", "<cmd>lua vim.diagnostic.open_float()<CR>" })
+nmap({ "gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>" })
+nmap({ "gn", "<cmd>lua vim.diagnostic.goto_next()<CR>" })
+-- nmap({'n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>'})
+nmap({ "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>" })
+nmap({ "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>" })
+-- nmap({ '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>'})
+-- nmap({ '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>'})
+-- nmap({ '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>'})
+-- nmap({ '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>'})
