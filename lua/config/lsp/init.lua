9eafd697f5e14c33665f9a2fdc7837efe45dec95
XXiaoA <isxxiaoa@gmail.com>
2022-07-30 23:28:06 +0800

    update

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -12,15 +12,23 @@ use({
 })
 use({ "williamboman/mason-lspconfig.nvim" })
 
+use({
+    "ii14/emmylua-nvim",
+    event = "VimEnter",
+})
+
 -- lsp_signature
-use({ "ray-x/lsp_signature.nvim", config = "require('config.lsp.lsp-signature')" })
+use({
+    "ray-x/lsp_signature.nvim",
+    after = "emmylua-nvim",
+    config = "require('config.lsp.lsp-signature')",
+})
 
 use({
     "glepnir/lspsaga.nvim",
     branch = "main",
+    after = "emmylua-nvim",
     config = [[require("config.lsp.lspsaga")]],
 })
-
-use({ "ii14/emmylua-nvim" })
 
 require("config.lsp.setup")
