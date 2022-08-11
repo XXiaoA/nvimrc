bfdbf010ea19e9773cc723bb98dec4d7e7c22065
XXiaoA <isxxiaoa@gmail.com>
2022-08-11 16:41:37 +0800

    disable illuminate in dashboard

--- lua/config/lsp/illuminate.lua
+++ lua/config/lsp/illuminate.lua
@@ -3,4 +3,4 @@ local nmap = require("core.keymap").set_keymap("n")
 nmap("<A-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>')
 nmap("<A-p>", '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>')
 
-vim.g.Illuminate_ftblacklist = { "neo-tree", "aerial" }
+vim.g.Illuminate_ftblacklist = { "neo-tree", "aerial", "dashboard" }
