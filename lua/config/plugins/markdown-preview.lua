75d65858bcdbf8447fc478aa59597ad5de49c33b
XXiaoA <isxxiaoa@gmail.com>
2022-08-17 23:23:30 +0800

    save

--- lua/config/plugins/markdown-preview.lua
+++ lua/config/plugins/markdown-preview.lua
@@ -1,9 +1,9 @@
 vim.g.mkdp_auto_close = 0
 
--- open in new window
-vim.cmd([[
-function OpenMarkdownPreview (url)
-  execute "silent ! google-chrome --new-window " . a:url
-endfunction
-let g:mkdp_browserfunc = 'OpenMarkdownPreview'
-]])
+-- -- open in new window
+-- vim.cmd([[
+-- function OpenMarkdownPreview (url)
+--   execute "silent ! google-chrome --new-window " . a:url
+-- endfunction
+-- let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+-- ]])
