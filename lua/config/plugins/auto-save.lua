3428558e925a81555ef2ec766d25f7c2a2468415
XXiaoA <isxxiaoa@gmail.com>
2022-08-17 21:57:49 +0800

    feat(auto-save): disable auto-save if files doesn't exist

--- lua/config/plugins/auto-save.lua
+++ lua/config/plugins/auto-save.lua
@@ -4,5 +4,18 @@ if not as then
 end
 
 as.setup({
+    condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        local file = vim.api.nvim_buf_get_name(0)
+        if
+            fn.getbufvar(buf, "&modifiable") == 1
+            and utils.not_in(fn.getbufvar(buf, "&filetype"), {})
+            and fn.findfile(file, ".") ~= ""
+        then
+            return true -- met condition(s), can save
+        end
+        return false -- can't save
+    end,
     disabled_patterns = { "wezterm.lua" },
 })
