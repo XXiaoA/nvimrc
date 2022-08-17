c7dac34211398a2c5a39d947cbe6acec8c2f4653
XXiaoA <isxxiaoa@gmail.com>
2022-08-17 18:15:44 +0800

    save

--- lua/config/plugins/nvim-autopairs.lua
+++ lua/config/plugins/nvim-autopairs.lua
@@ -1,16 +1,19 @@
 local npairs = require("utils").requirePlugin("nvim-autopairs")
-local Rule = require("utils").requirePlugin("nvim-autopairs.rule")
-local ts_conds = require("utils").requirePlugin("nvim-autopairs.ts-conds")
-
-if npairs and Rule and ts_conds then
-    npairs.setup({
-        check_ts = true,
-    })
+if not npairs then
+    return
 end
+
+npairs.setup({
+    check_ts = true,
+})
 
 -- If you want insert `(` after select function or method item
-local cmp_autopairs = require("utils").requirePlugin("nvim-autopairs.completion.cmp")
 local cmp = require("utils").requirePlugin("cmp")
-if cmp_autopairs and cmp then
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+if not cmp then
+    return
 end
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
