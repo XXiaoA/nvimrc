9b6071a379ab57f78827f162779ea4f73d54ef48
XXiaoA <isxxiaoa@gmail.com>
2022-08-19 18:28:48 +0800

    BREAKING CHANGE: formatter use default configurations

--- lua/config/plugins/formatter.lua
+++ lua/config/plugins/formatter.lua
@@ -1,69 +1,30 @@
 local formatter = require("utils").requirePlugin("formatter")
-
 if not formatter then
     return
 end
 
-local lua_config = {
-    function()
-        return {
-            exe = "stylua",
-            args = {
-                "--config-path " .. os.getenv("XDG_CONFIG_HOME") .. "/nvim/.stylua.toml",
-                "-",
-            },
-            stdin = true,
-        }
-    end,
-}
-
-local python_config = {
-    function()
-        return {
-            exe = "black",
-            args = { "-" },
-            stdin = true,
-        }
-    end,
-}
-
-local cpp_config = {
-    function()
-        return {
-            exe = "clang-format",
-            args = {
-                "-style=file:" .. os.getenv("XDG_CONFIG_HOME") .. "/nvim/.clang-format",
-                "-",
-            },
-            stdin = true,
-        }
-    end,
-}
-
-local fish_config = {
-    function()
-        return {
-            exe = "fish_indent",
-            stdin = true,
-        }
-    end,
-}
-
-local rust_config = {
-    function()
-        return {
-            exe = "rustfmt",
-            stdin = true,
-        }
-    end,
-}
-
 formatter.setup({
+    logging = true,
+    log_level = vim.log.levels.WARN,
     filetype = {
-        lua = lua_config,
-        python = python_config,
-        cpp = cpp_config,
-        fish = fish_config,
-        rust = rust_config,
+        lua = {
+            require("formatter.filetypes.lua").stylua,
+        },
+        python = {
+            require("formatter.filetypes.python").black,
+        },
+        cpp = {
+            require("formatter.filetypes.cpp").clangformat,
+        },
+        fish = {
+            require("formatter.filetypes.fish").fishindent,
+        },
+        rust = {
+            require("formatter.filetypes.rust").rustfmt,
+        },
+
+        ["*"] = {
+            require("formatter.filetypes.any").remove_trailing_whitespace,
+        },
     },
 })
