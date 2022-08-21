67499077a76aea3cb008a3af3d367bb7e21bfc16
XXiaoA <isxxiaoa@gmail.com>
2022-08-21 23:03:13 +0800

    save

--- lua/config/colorscheme/init.lua
+++ lua/config/colorscheme/init.lua
@@ -5,6 +5,7 @@ local add_colorscheme = colorscheme.add_colorscheme
 -- 主题
 add_colorscheme("gruvbox-material", { "sainnhe/gruvbox-material" })
 add_colorscheme("nightfox", { "EdenEast/nightfox.nvim", run = ":NightfoxCompile" })
+add_colorscheme("duskfox")
 add_colorscheme("catppuccin", { "catppuccin/nvim", as = "catppuccin" })
 
 local theme = utils.readConfig("color_scheme")
