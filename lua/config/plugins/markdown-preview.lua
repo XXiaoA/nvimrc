dc01e8b82a571a681c90cfab9079d2c1909156b5
XXiaoA <isxxiaoa@gmail.com>
2022-08-22 16:13:57 +0800

    feat(markdown-preview): open chrome with minimal layout

--- lua/config/plugins/markdown-preview.lua
+++ lua/config/plugins/markdown-preview.lua
@@ -1,9 +1,10 @@
 vim.g.mkdp_auto_close = 0
 
--- -- open in new window
--- vim.cmd([[
--- function OpenMarkdownPreview (url)
---   execute "silent ! google-chrome --new-window " . a:url
--- endfunction
--- let g:mkdp_browserfunc = 'OpenMarkdownPreview'
--- ]])
+-- open in new window
+vim.cmd([[
+function OpenMarkdownPreview (url)
+  execute "silent ! google-chrome-stable --new-window --app=" . a:url
+
+endfunction
+let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+]])
