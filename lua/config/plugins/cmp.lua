9394019e2b15c6d977491cc79180451e9bf2aa6d
XXiaoA <isxxiaoa@gmail.com>
2022-08-28 15:57:02 +0800

    cmp: change the menu appearance

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -17,6 +17,18 @@ map({ "i", "s" })("<C-l>", function()
 end)
 
 cmp.setup({
+    enabled = function()
+        -- disable completion in comments
+        local context = require("cmp.config.context")
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == "c" then
+            return true
+        else
+            return not context.in_treesitter_capture("comment")
+                and not context.in_syntax_group("Comment")
+        end
+    end,
+
     experimental = {
         ghost_text = true,
     },
@@ -83,20 +95,16 @@ cmp.setup({
 
     -- 使用lspkind-nvim显示类型图标
     formatting = {
+        fields = { "kind", "abbr", "menu" },
         format = lspkind.cmp_format({
-            with_text = true, -- do not show text alongside icons
+            with_text = false, -- do not show text alongside icons
             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            before = function(entry, vim_item)
-                -- Source 显示提示来源
-                if entry.source.name == "nvim_lsp" then
-                    vim_item.menu = "[LSP]"
-                elseif entry.source.name == "nvim_lsp_signature_help" then
-                    vim_item.menu = ""
-                else
-                    vim_item.menu = "[" .. string.upper(entry.source.name) .. "]"
-                end
-                return vim_item
-            end,
+            menu = {
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[LuaSnip]",
+                nvim_lua = "[Lua]",
+            },
         }),
     },
 })
