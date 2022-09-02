f40cb108a0a185cf2c1a8fc241ff166eb81a77f7
XXiaoA <isxxiaoa@gmail.com>
2022-09-02 17:06:31 +0800

    colorscheme

--- lua/config/colorscheme/catppuccin.lua
+++ lua/config/colorscheme/catppuccin.lua
@@ -5,6 +5,20 @@ require("catppuccin").setup({
         enabled = true,
         path = vim.fn.stdpath("cache") .. "/catppuccin",
     },
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = { "bold" },
+        keywords = { "italic" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
     integrations = {
         neotree = {
             enabled = true,
---
+++ lua/config/colorscheme/duskfox.lua
@@ -0,0 +1,16 @@
+require("nightfox").setup({
+    options = {
+        styles = {
+            comments = "italic",
+            conditionals = "italic",
+            constants = "NONE",
+            functions = "bold",
+            keywords = "italic",
+            numbers = "NONE",
+            operators = "NONE",
+            strings = "NONE",
+            types = "NONE",
+            variables = "NONE",
+        },
+    },
+})
