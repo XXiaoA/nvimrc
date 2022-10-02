6b435f370f1cf8edc50576d22b82858ff0dfb272
XXiaoA <isxxiaoa@gmail.com>
2022-10-02 22:37:37 +0800

    bump to v0.9

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local utils = require("utils")
 local NVIM_VERSION = utils.get_nvim_version()
 
-if NVIM_VERSION ~= "0.8.0" then
+if NVIM_VERSION ~= "0.9.0" then
     vim.api.nvim_err_writeln("Your nvim version is unexpected")
 else
     require("core")
