98a023214e2323fd3e3a36db00f6c12b44cddbfa
XXiaoA <isxxiaoa@gmail.com>
2022-11-27 00:00:27 +0800

    feat(lsp): add capabilities of completion (for nvim-cmp)

--- lua/config/lsp/setup.lua
+++ lua/config/lsp/setup.lua
@@ -90,6 +90,38 @@ capabilities.textDocument.foldingRange = {
     lineFoldingOnly = true,
 }
 
+capabilities.textDocument.completion = {
+    completionItem = {
+        commitCharactersSupport = true,
+        deprecatedSupport = true,
+        insertReplaceSupport = true,
+        insertTextModeSupport = {
+            valueSet = { 1, 2 },
+        },
+        labelDetailsSupport = true,
+        preselectSupport = true,
+        resolveSupport = {
+            properties = { "documentation", "detail", "additionalTextEdits" },
+        },
+        snippetSupport = true,
+        tagSupport = {
+            valueSet = { 1 },
+        },
+    },
+    completionList = {
+        itemDefaults = {
+            "commitCharacters",
+            "editRange",
+            "insertTextFormat",
+            "insertTextMode",
+            "data",
+        },
+    },
+    contextSupport = true,
+    dynamicRegistration = false,
+    insertTextMode = 1,
+}
+
 local settings = {
     on_attach = on_attach,
     capabilities = capabilities,
