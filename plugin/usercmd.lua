397dd28ba7c0ec36f2eec177bb94f4c105da2e67
XXiaoA <isxxiaoa@gmail.com>
2022-12-30 19:10:01 +0800

    `XXiaoA all_colorschemes` -> `XXiaoA colorschemes`

--- plugin/usercmd.lua
+++ plugin/usercmd.lua
@@ -1,15 +1,15 @@
 local api = vim.api
 
 api.nvim_create_user_command("XXiaoA", function(ctx)
-    if ctx.args == "all_colorschemes" then
-        vim.pretty_print(vim.tbl_keys(require("core.colorscheme").all_colorschemes))
+    if ctx.args == "colorschemes" then
+        vim.pretty_print(require("core.colorscheme").all_colorschemes)
     elseif ctx.args == "config" then
         vim.cmd.e(vim.fn.stdpath("config") .. "/config.yml")
     end
 end, {
     nargs = 1,
     complete = function(arg)
-        local list = { "all_colorschemes", "config" }
+        local list = { "colorschemes", "config" }
         return vim.tbl_filter(function(s)
             return string.match(s, "^" .. arg)
         end, list)
