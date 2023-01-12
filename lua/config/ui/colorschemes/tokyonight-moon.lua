0b41c5c72d7b5d61796948440749110e7ddeda17
XXiaoA <isxxiaoa@gmail.com>
2023-01-12 18:11:43 +0800

    colorschemes(tokyonight): bold the lualine

--- lua/config/ui/colorschemes/tokyonight-moon.lua
+++ lua/config/ui/colorschemes/tokyonight-moon.lua
@@ -17,5 +17,5 @@ require("tokyonight").setup({
     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
     dim_inactive = false, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 })
