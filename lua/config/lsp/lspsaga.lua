9b9f678b957106518526896229ed6e8f337c9c0f
XXiaoA <isxxiaoa@gmail.com>
2023-01-13 14:36:27 +0800

    pin the lspsaga

--- lua/config/lsp/lspsaga.lua
+++ lua/config/lsp/lspsaga.lua
@@ -1,6 +1,7 @@
 return {
     "glepnir/lspsaga.nvim",
     event = "VeryLazy",
+    commit = "b7b4777369b441341b2dcd45c738ea4167c11c9e",
     config = function()
         local saga = require("lspsaga")
         saga.init_lsp_saga({
