fbefed24b5a5ada7ae8c67113cba6f1fa3a9daf5
XXiaoA <isxxiaoa@gmail.com>
2023-01-20 17:01:00 +0800

    refactor: merge lsp.lsp and lsp.init

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,5 +1,48 @@
+local yamler = require("utils.yamler")
+
 return {
     {
+        "neovim/nvim-lspconfig",
+        -- TODO: prefect lazy load
+        event = "BufReadPre",
+        ---@diagnostic disable-next-line: param-type-mismatch
+        ft = vim.tbl_keys(yamler.get_value("lsp")),
+        dependencies = {
+            "mason.nvim",
+            "mason-lspconfig.nvim",
+            "null-ls.nvim",
+            "simrat39/rust-tools.nvim",
+        },
+    },
+
+    {
+        "williamboman/mason.nvim",
+        branch = "main",
+        opts = {
+            ui = {
+                border = "single",
+            },
+        },
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            local mason_lspconfig = require("utils").require("mason-lspconfig")
+            if mason_lspconfig then
+                mason_lspconfig.setup({
+                    -- TODO: prefect lazy load
+                    ---@diagnostic disable-next-line: param-type-mismatch
+                    ensure_installed = vim.tbl_values(yamler.get_value("lsp")),
+                })
+            end
+
+            require("config.lsp.setup")
+        end,
+    },
+
+    {
+        -- TODO: perfect it
         -- workspace library for sumneko_lua
         "ii14/emmylua-nvim",
     },
