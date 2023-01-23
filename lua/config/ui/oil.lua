fa3a9b77a12326792c1f4b92220f3027348b6bc7
XXiaoA <isxxiaoa@gmail.com>
2023-01-23 22:45:51 +0800

    fix(oil): C-s to save instead of vsplit

--- lua/config/ui/oil.lua
+++ lua/config/ui/oil.lua
@@ -14,6 +14,7 @@ return {
         keymaps = {
             ["q"] = "actions.close",
             ["<M-m>"] = "actions.close",
+            ["<C-s>"] = false,
         },
         view_options = {
             show_hidden = true,
