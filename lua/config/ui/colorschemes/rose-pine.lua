9606f822b206c5fd085dffa1ee2d2fbe06018eb6
XXiaoA <isxxiaoa@gmail.com>
2023-02-25 20:24:33 +0800

    colorschemes(rose-pine): transparent telescope.nvim

--- lua/config/ui/colorschemes/rose-pine.lua
+++ lua/config/ui/colorschemes/rose-pine.lua
@@ -17,4 +17,13 @@ require("rose-pine").setup({
     disable_background = false,
     disable_float_background = false,
     disable_italics = false,
+
+    highlight_groups = {
+        TelescopeBorder = { fg = "highlight_high", bg = "none" },
+        TelescopeNormal = { bg = "none" },
+        TelescopePromptNormal = { bg = "base" },
+        TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+        TelescopeSelection = { fg = "text", bg = "base" },
+        TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+    },
 })
