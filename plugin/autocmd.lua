e8580c7c91f8369aa6d4ed6c28afda0562b4d945
XXiaoA <isxxiaoa@gmail.com>
2023-05-02 00:02:50 +0800

    fix: prevent entering buffers in insert mode. https://github.com/nvim-telescope/telescope.nvim/issues/2027

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -183,3 +183,18 @@ au("User", {
         utils.require("scripts")
     end,
 })
+
+-- Prevent entering buffers in insert mode.
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2027
+au("WinLeave", {
+    group = xxiaoa_group,
+    callback = function()
+        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(
+                vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
+                "i",
+                false
+            )
+        end
+    end,
+})
