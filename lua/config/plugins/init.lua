00b5ff10f7583392301db68461174ef5521e5969
XXiaoA <isxxiaoa@gmail.com>
2023-05-17 16:58:35 +0800

    fix(mini): lazy now supports setup mini plugins smartly

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -33,23 +33,21 @@ return {
             { mode = { "n", "x" }, "<M-down>" },
             { mode = { "n", "x" }, "<M-up>" },
         },
-        config = function()
-            require("mini.move").setup({
-                mappings = {
-                    -- Move visual selection in Visual mode.
-                    left = "<M-left>",
-                    right = "<M-right>",
-                    down = "<M-down>",
-                    up = "<M-up>",
+        opts = {
+            mappings = {
+                -- Move visual selection in Visual mode.
+                left = "<M-left>",
+                right = "<M-right>",
+                down = "<M-down>",
+                up = "<M-up>",
 
-                    -- Move current line in Normal mode
-                    line_left = "<M-left>",
-                    line_right = "<M-right>",
-                    line_down = "<M-down>",
-                    line_up = "<M-up>",
-                },
-            })
-        end,
+                -- Move current line in Normal mode
+                line_left = "<M-left>",
+                line_right = "<M-right>",
+                line_down = "<M-down>",
+                line_up = "<M-up>",
+            },
+        },
     },
 
     {
