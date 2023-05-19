2142bd1b4469e6535c5b8d24667ff3d02a4d3e3f
XXiaoA <isxxiaoa@gmail.com>
2023-05-19 21:01:46 +0800

    feat(neotree): add some keymaps

--- lua/config/ui/neotree.lua
+++ lua/config/ui/neotree.lua
@@ -43,6 +43,37 @@ return {
             mappings = {
                 ["<space>"] = "none",
                 ["-"] = "navigate_up",
+                -- open file without losing focus
+                ["<tab>"] = function(state)
+                    local node = state.tree:get_node()
+                    if require("neo-tree.utils").is_expandable(node) then
+                        state.commands["toggle_node"](state)
+                    else
+                        state.commands["open"](state)
+                        vim.cmd("Neotree reveal")
+                    end
+                end,
+                h = function(state)
+                    local node = state.tree:get_node()
+                    if node.type == "directory" and node:is_expanded() then
+                        require("neo-tree.sources.filesystem").toggle_directory(state, node)
+                    else
+                        require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+                    end
+                end,
+                l = function(state)
+                    local node = state.tree:get_node()
+                    if node.type == "directory" then
+                        if not node:is_expanded() then
+                            require("neo-tree.sources.filesystem").toggle_directory(state, node)
+                        elseif node:has_children() then
+                            require("neo-tree.ui.renderer").focus_node(
+                                state,
+                                node:get_child_ids()[1]
+                            )
+                        end
+                    end
+                end,
             },
         },
         default_component_configs = {
