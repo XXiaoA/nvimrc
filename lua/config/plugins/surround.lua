a7b3ec35c01b5f1d95673d52496df96b53f023f9
XXiaoA <isxxiaoa@gmail.com>
2023-05-26 21:48:09 +0800

    feat(nvim-surround): remove surrounds `q` and `i` , add `?`

--- lua/config/plugins/surround.lua
+++ lua/config/plugins/surround.lua
@@ -3,16 +3,9 @@ local M = {
     event = "VeryLazy",
 }
 
-M.config = function()
-    local surround = require("utils").require("nvim-surround")
-    if not surround then
-        return
-    end
+M.opts = function()
     local config = require("nvim-surround.config")
-    local buffer = require("nvim-surround.buffer")
-    local utils = require("nvim-surround.utils")
-
-    surround.setup({
+    return {
         keymaps = {
             insert = "<C-g>s",
             insert_line = "<C-g>S",
@@ -26,49 +19,23 @@ M.config = function()
             change = "sr",
         },
         surrounds = {
-            ["q"] = {
+            -- remove the default configuration
+            ["i"] = false,
+
+            ["?"] = { -- TODO: Add find/delete/change functions
                 add = function()
-                    local quotes = { '"', "'", "`" }
-                    local selection_list = {}
-                    local not_inside = true
-                    for _, quote in ipairs(quotes) do
-                        local cur_selection = config.get_selection({ motion = "a" .. quote })
-                        if not cur_selection then
-                            goto continue
-                        end
-                        cur_selection = {
-                            left = {
-                                first_pos = cur_selection.first_pos,
-                                last_pos = cur_selection.first_pos,
-                            },
-                            right = {
-                                first_pos = cur_selection.last_pos,
-                                last_pos = cur_selection.last_pos,
-                            },
-                        }
-                        local cursor = buffer.get_curpos()
-                        if buffer.is_inside(cursor, cur_selection) then
-                            table.insert(selection_list, cur_selection)
-                            not_inside = false
-                        end
-                        ::continue::
-                    end
-                    if not_inside then
-                        return { { quotes[1] }, { quotes[1] } }
-                    else
-                        local best_selection = utils.filter_selections_list(selection_list)
-                        local best_text = buffer.get_text(best_selection.left)
-                        local next_quote = vim.tbl_filter(function(value)
-                            if value ~= best_text[1] then
-                                return value
-                            end
-                        end, quotes)[1]
-                        return { { next_quote }, { next_quote } }
+                    local left_delimiter = config.get_input("Enter the left delimiter: ")
+                    local right_delimiter = left_delimiter
+                        and config.get_input("Enter the right delimiter: ")
+                    if right_delimiter then
+                        return { { left_delimiter }, { right_delimiter } }
                     end
                 end,
+                find = function() end,
+                delete = function() end,
             },
         },
-    })
+    }
 end
 
 return M
