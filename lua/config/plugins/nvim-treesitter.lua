e2874243968e94dca2ae941b426aa0178f8093d1
XXiaoA <isxxiaoa@gmail.com>
2023-05-26 21:32:34 +0800

    feat(textobject): use `nvim-treesitter-textobjects` and `mini.ai` took them from LazyVim

--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -99,4 +99,26 @@ return {
             end
         end,
     },
+
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = "nvim-treesitter",
+        init = function()
+            -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+            local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+            local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+            local enabled = false
+            if opts.textobjects then
+                for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+                    if opts.textobjects[mod] and opts.textobjects[mod].enable then
+                        enabled = true
+                        break
+                    end
+                end
+            end
+            if not enabled then
+                require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+            end
+        end,
+    },
 }
