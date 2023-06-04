415aa318a42c162cc9d95419d6e105206cf64cf5
XXiaoA <isxxiaoa@gmail.com>
2023-06-04 22:08:06 +0800

    perf(illuminate): on large files >2000 lines, enable lsp references only https://github.com/LazyVim/LazyVim/commit/08422bf559cb74a2e673e60d551269cbcb2e18df

--- lua/config/lsp/illuminate.lua
+++ lua/config/lsp/illuminate.lua
@@ -5,6 +5,10 @@ return {
         require("illuminate").configure({
             delay = 200,
             modes_allowlist = { "n" },
+            large_file_cutoff = 2000,
+            large_file_overrides = {
+                providers = { "lsp" },
+            },
         })
     end,
     keys = {
