e01c4a2703e202e41375164e06c793d2c5611083
XXiaoA <isxxiaoa@gmail.com>
2023-07-13 22:46:20 +0800

    fix(keymaps): resolve conflict between `K` keymap we have a `K` keymap in `ufo.lua` before

--- lua/config/lsp/keymaps.lua
+++ lua/config/lsp/keymaps.lua
@@ -9,7 +9,6 @@ function M.get()
         { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
         { "gI", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
         { "gt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
-        { "K", vim.lsp.buf.hover, desc = "Hover" },
         { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
         { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
         { "gp", "<cmd>Aphrodite peek_definition<CR>", "Peek Definition" },
