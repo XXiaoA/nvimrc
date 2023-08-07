26343fc906f68d18981a31721c06a1688e21fe76
XXiaoA <isxxiaoa@gmail.com>
2023-08-07 15:54:45 +0800

    feat(snippets): update `date` format and add `forin` for cpp

--- snippets/cpp.lua
+++ snippets/cpp.lua
@@ -19,7 +19,19 @@ return {
         i(1),
         i = f(function(args) return args[1][1] end, 1),
         i(2),
-        i(3)
-    })
-    )
+        i(0)
+    })),
+
+    s("forin", fmta([[
+    for (auto <var> : <array>) {
+        <>
+    }
+    ]], {
+        var = f(function(args)
+            local user_input = args[1][1]
+            return user_input:sub(-1) == "s" and user_input:sub(1, -2) or user_input .. "_key"
+        end, 1),
+        array = i(1, "array"),
+        i(0),
+    }))
 }
