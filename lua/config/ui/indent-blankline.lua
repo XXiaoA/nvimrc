83e35d2c63e088e4de7680cca3b566dee20b6b28
XXiaoA <isxxiaoa@gmail.com>
2023-08-11 12:07:53 +0800

    refactor: make `indent_blankline` configuration clean

--- lua/config/ui/indent-blankline.lua
+++ lua/config/ui/indent-blankline.lua
@@ -1,22 +1,17 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     event = "BufReadPre",
-    config = function()
-        local indent_blankline = require("utils").require("indent_blankline")
-        if indent_blankline then
-            indent_blankline.setup({
-                space_char_blankline = " ",
-                show_current_context = true,
-                show_current_context_start = true,
-            })
-        end
-        vim.g.indent_blankline_filetype_exclude = {
+    opts = {
+        space_char_blankline = " ",
+        show_current_context = true,
+        show_current_context_start = true,
+        filetype_exclude = {
             "help",
             "TERMINAL",
             "terminal",
             "startuptime",
             "toggleterm",
             "translator",
-        }
-    end,
+        },
+    },
 }
