41f2f98bca314858f9a040bec48d49ae17b6d07a
XXiaoA <isxxiaoa@gmail.com>
2023-08-13 14:17:21 +0800

    remove autocmd for telescope it seems that the bug has fixed

--- plugin/autocmd.lua
+++ plugin/autocmd.lua
@@ -1,4 +1,4 @@
----@diagnostic disable: param-type-mismatch
+---@diagnostic disable: param-type-mismatch, missing-fields
 
 local utils = require("utils")
 local api = vim.api
@@ -185,20 +185,5 @@ au("User", {
     group = xxiaoa_group,
     callback = function()
         utils.require("misc")
-    end,
-})
-
--- Prevent entering buffers in insert mode.
--- https://github.com/nvim-telescope/telescope.nvim/issues/2027
-au("WinLeave", {
-    group = xxiaoa_group,
-    callback = function()
-        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-            vim.api.nvim_feedkeys(
-                vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
-                "i",
-                false
-            )
-        end
     end,
 })
