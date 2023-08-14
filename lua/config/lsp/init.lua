ffc79fabf75969e44d4ac1cff8ce5356d785db98
XXiaoA <isxxiaoa@gmail.com>
2023-08-14 17:27:56 +0800

    feat(lsp): make `inc-rename` work with `dressing.nvim`

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -52,6 +52,8 @@ return {
     {
         "smjonas/inc-rename.nvim",
         cmd = "IncRename",
-        config = true,
+        opts = {
+            input_buffer_type = "dressing",
+        },
     },
 }
