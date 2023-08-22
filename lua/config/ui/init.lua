6f1e9052e827f0d62c6750133675c090f65af7fb
XXiaoA <isxxiaoa@gmail.com>
2023-08-22 23:39:59 +0800

    feat(colorscheme): add `tokyonight-night`

--- lua/config/ui/init.lua
+++ lua/config/ui/init.lua
@@ -14,7 +14,7 @@ return {
     {
         "folke/tokyonight.nvim",
         init = function()
-            add_colorscheme("tokyonight-storm", "tokyonight-moon")
+            add_colorscheme("tokyonight-storm", "tokyonight-moon", "tokyonight-night")
         end,
     },
 
