d4a84954b018080f095da7ab9421e1e24e6978f3
XXiaoA <isxxiaoa@gmail.com>
2023-10-03 21:18:44 +0800

    feat: update template for OI

--- lua/misc/usercmd.lua
+++ lua/misc/usercmd.lua
@@ -15,14 +15,30 @@ api.nvim_create_user_command("XXiaoA", function(ctx)
                 "#include <iostream>",
                 "using namespace std;",
                 "",
+                [[int read() {]],
+                [[int x = 0, w = 1;]],
+                [[char ch = 0;]],
+                [[while (ch < '0' || ch > '9') {]],
+                [[if (ch == '-')]],
+                [[w = -1;]],
+                [[ch = getchar();,]],
+                [[}]],
+                [[while (ch >= '0' && ch <= '9') {]],
+                [[x = x * 10 + (ch - '0');]],
+                [[ch = getchar();]],
+                [[}]],
+                [[return x * w;]],
+                [[}]],
+                "",
                 "int main() {",
                 "   ios::sync_with_stdio(false);",
                 "   int n;",
                 "   cin >> n;",
                 "",
-                "\treturn 0;",
+                "   return 0;",
                 "}",
             })
+            vim.cmd("w!")
         end
     else
         vim.print("no command " .. ctx.args)
