67f35b5fd272885946295bcf66044eead0879092
XXiaoA <isxxiaoa@gmail.com>
2023-10-14 00:34:46 +0800

    feat(templates): update for cpp

--- templates/oi.cpp
+++ templates/oi.cpp
@@ -2,24 +2,22 @@
 using namespace std;
 
 int read() {
-  int x = 0, w = 1;
-  char ch = 0;
-  while (ch < '0' || ch > '9') {
-    if (ch == '-')
-      w = -1;
-    ch = getchar();
-  }
-  while (ch >= '0' && ch <= '9') {
-    x = x * 10 + (ch - '0');
-    ch = getchar();
-  }
-  return x * w;
+    int x = 0, w = 1;
+    char ch = 0;
+    // clang-format off
+    while (ch < '0' || ch > '9') { if (ch == '-') w = -1; ch = getchar(); }
+    while (ch >= '0' && ch <= '9') { x = x * 10 + (ch - '0'); ch = getchar(); }
+    // clang-format on
+    return x * w;
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  int n;
-  n = read();
+#ifdef LOCAL
+    freopen("in", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    int n;
+    n = read();
 
-  return 0;
+    return 0;
 }
