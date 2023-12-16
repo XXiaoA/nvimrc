b5f5b96559f5b60d8dd024742650f0e6fcfba691
XXiaoA <isxxiaoa@gmail.com>
2023-12-16 15:29:51 +0800

    feat(lsp): `fidget.nvim` use latest version

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -34,15 +34,8 @@ return {
 
     {
         "j-hui/fidget.nvim",
-        tag = "legacy",
         event = "LspAttach",
-        opts = {
-            sources = {
-                ["pylsp"] = {
-                    ignore = true,
-                },
-            },
-        },
+        opts = {},
     },
 
     {
