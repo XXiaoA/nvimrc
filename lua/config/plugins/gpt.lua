8c16606eea985d847c7870134581d50b13ff6ee7
XXiaoA <isxxiaoa@gmail.com>
2024-02-03 13:19:48 +0800

    feat: add keymaps for `gp.nvim`

--- lua/config/plugins/gpt.lua
+++ lua/config/plugins/gpt.lua
@@ -1,5 +1,18 @@
 return {
     "robitx/gp.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        chat_shortcut_respond = { modes = { "n", "x" }, shortcut = "<leader>r" },
+        chat_shortcut_delete = { modes = { "n", "x" }, shortcut = "<leaderd" },
+        chat_shortcut_stop = { modes = { "n", "x" }, shortcut = "<leader>s" },
+        chat_shortcut_new = { modes = { "n", "x" }, shortcut = "<leader>n" },
+    },
+    keys = {
+        { "<leader>pn", "<Cmd>GpChatNew split<CR>", desc = "New Chat" },
+        { "<leader>pn", ":<C-u>'<,'>GpChatNew split<CR>", mode = { "x" }, desc = "New Chat" },
+        { "<leader>pt", "<Cmd>GpChatToggle split<CR>", desc = "Toggle Chat" },
+        { "<leader>pt", ":<C-u>'<,'>GpChatToggle split<CR>", mode = { "x" }, desc = "Toggle Chat" },
+        { "<leader>pf", "<Cmd>GpChatFinder<CR>", desc = "Find Chat" },
+        { "<leader>pp", ":<C-u>'<,'>GpChatPaste<CR>", mode = { "x" }, desc = "Chat Paste" },
+    },
 }
