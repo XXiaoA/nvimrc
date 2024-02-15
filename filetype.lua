a976e91bcf5a8eebe5e71bf42cf93e6c50e21e89
XXiaoA <isxxiaoa@gmail.com>
2024-02-15 20:38:43 +0800

    feat: use `filetype.lua` instead ftdetect

---
+++ filetype.lua
@@ -0,0 +1,5 @@
+vim.filetype.add({
+    extension = {
+        rasi = "css",
+    },
+})
--- ftdetect/rasi.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.rasi",
-    command = "set filetype=css",
-})
