68c113b7a5d902463eaa3e333bf4da12912dedb0
XXiaoA <isxxiaoa@gmail.com>
2024-03-09 14:00:52 +0800

    feat: keymaps for `noice.nvim`

--- lua/config/ui/noice.lua
+++ lua/config/ui/noice.lua
@@ -74,7 +74,8 @@ return {
         { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
         { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
         { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
-        { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+        { "<leader>nt", function() require("noice").cmd("telescope") end, desc = "Noice Telescope" },
+        { "<leader>nn", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
         { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
         { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}}},
 }
