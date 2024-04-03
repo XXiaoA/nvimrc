df704a81c8110e276304059aafd2c90b89795386
XXiaoA <isxxiaoa@gmail.com>
2024-04-03 22:57:40 +0800

    adjust options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -31,73 +31,44 @@ o.mouse = "nv" -- "a"
 o.backupdir = cache_dir .. "backup/"
 -- open swap
 o.swapfile = false
--- 编码设置
 o.encoding = "utf-8"
--- 加快响应速度
 o.updatetime = 100
--- 连击频率
 o.timeoutlen = 500
 o.timeout = true
 o.ttimeout = true
 o.ttimeoutlen = 10
--- 显示命令字符
 o.showcmd = true
--- always show tabline
+-- show tabline if necessary
 o.showtabline = 1
 -- use global line
 o.laststatus = 3
--- 允许当前窗口未保存时切换到其他窗口
 o.hidden = true
--- 启用 24-bit 色
 o.termguicolors = true
--- 语法高亮
 o.syntax = "enable"
--- 高亮当前行
 o.cursorline = true
--- 设置行号
 o.number = true
--- 显示行尾空格
 o.list = true
--- 设置自动折行
 o.wrap = true
--- 取消自动换行
 o.textwidth = 0
--- jk移动时光标下上方保留8行
 o.scrolloff = 10
 o.sidescrolloff = 8
--- 使用系统剪切板
 o.clipboard = "unnamedplus"
--- 设置自动缩进
 o.autoindent = true
--- 将缩进转换为空格
 o.expandtab = true
--- 设置 tab 缩进显示宽度
 o.tabstop = 4
--- 设置 tab 缩进实际宽度
 o.softtabstop = 4
--- 设置>> <<缩进数量
 o.shiftwidth = 4
--- 高亮搜索结果
 o.hlsearch = true
--- 高亮匹配符号
-o.showmatch = true
--- 开启命令补全
-o.wildmenu = true
--- 忽略大小写
+o.showmatch = false
+o.wildmenu = false
 o.ignorecase = true
--- 智能大小写
 o.smartcase = true
--- 打开拼写检查
 o.spell = false
--- 设置拼写检查语言
 o.spelllang = { "en_us", "cjk" }
 o.spelloptions = "camel"
 
--- 是否支持折叠
 o.foldenable = true
--- 折叠的方式
 o.foldmethod = "manual"
--- 折叠的级别（100）
 o.foldlevel = 100
 o.foldlevelstart = 100
 -- o.foldcolumn = "1"
@@ -129,7 +100,7 @@ o.smartindent = true -- Insert indents automatically
 o.splitbelow = true -- Put new windows below current
 o.splitright = true -- Put new windows right of current
 o.winminwidth = 5 -- Minimum window width
-o.pumheight = 14 -- limit the max height of windows
+o.pumheight = 14 -- Maximum number of items to show in the popup menu
 
 -- smarter diff algorithm
 -- https://vimways.org/2018/the-power-of-diff/
