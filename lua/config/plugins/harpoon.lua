a1153656c03d19ce23e5651cacda2a489d70dd42
XXiaoA <isxxiaoa@gmail.com>
2024-04-05 16:27:15 +0800

    fix(harpoon): follow the upstream

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
         {
             "<leader>H",
             function()
-                require("harpoon"):list():append()
+                require("harpoon"):list():add()
             end,
             desc = "Harpoon file",
         },
