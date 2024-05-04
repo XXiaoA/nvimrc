0660671362fafecc5e28e173bbcf9968e93febda
XXiaoA <isxxiaoa@gmail.com>
2024-05-04 16:34:11 +0800

    remove useless code for leap.nvim

--- lua/config/plugins/leap.lua
+++ lua/config/plugins/leap.lua
@@ -1,7 +1,6 @@
 local M = {
     "ggandor/leap.nvim",
     keys = {
-        { mode = { "n", "o", "x" }, "\\" },
         { mode = { "n", "o", "x" }, "<CR>" },
         { mode = { "n", "o", "x" }, "<S-CR>" },
         { mode = { "n", "o", "x" }, "g<CR>" },
@@ -25,11 +24,6 @@ local M = {
 
 M.config = function()
     vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
-    require("leap").add_default_mappings(true)
-
-    vim.keymap.del({ "n", "o", "x" }, "s")
-    vim.keymap.del({ "n", "o", "x" }, "S")
-    vim.keymap.del({ "n", "o", "x" }, "gs")
     vim.keymap.set({ "n", "o", "x" }, "<CR>", "<Plug>(leap-forward-to)")
     vim.keymap.set({ "n", "o", "x" }, "<S-CR>", "<Plug>(leap-backward-to)")
     vim.keymap.set({ "n", "o", "x" }, "g<CR>", "<Plug>(leap-from-window)")
