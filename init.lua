1e9dafebb3ff52dcb966135d4a4ea78e7805f836
XXiaoA <isxxiaoa@gmail.com>
2024-05-25 00:07:00 +0800

    feat!: bump neovim version to 0.11

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.loader.enable()
 
 local NVIM_VERSION = require("utils").get_nvim_version()
-if NVIM_VERSION ~= "0.10.0" then
+if NVIM_VERSION ~= "0.11.0" then
     vim.api.nvim_err_writeln("Your nvim version is unexpected")
 else
     require("core")
