092986f439b33bbdaf7e524e661fb6afa7b37799
XXiaoA <isxxiaoa@gmail.com>
2024-05-25 11:00:08 +0800

    feat(cmp): enable automatically adding parenthesis for pyright

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -134,7 +134,7 @@ M.config = function()
     -- Add parenthesis on completion confirmation
     cmp.event:on("confirm_done", function(event)
         local ok, ls_name = pcall(ls_name_from_event, event)
-        if ok and not vim.tbl_contains({ "lua_ls" }, ls_name) then
+        if ok and not vim.tbl_contains({ "lua_ls", "pyright" }, ls_name) then
             return
         end
 
