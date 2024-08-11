6e8865a326dc7ea51f434e7fe43df4b0003eea3d
XXiaoA <isxxiaoa@gmail.com>
2024-08-11 11:51:14 +0800

    feat(codeium): use <A-p> to switch to previous hint

--- lua/config/plugins/codeium.lua
+++ lua/config/plugins/codeium.lua
@@ -10,7 +10,7 @@ return {
         { mode = "i", "<A-w>", function() require("neocodeium").accept_word() end },
         { mode = "i", "<A-a>", function() require("neocodeium").accept_line() end },
         { mode = "i", "<A-n>", function() require("neocodeium").cycle_or_complete() end },
-        { mode = "i", "<A-N>", function() require("neocodeium").cycle_or_complete(-1) end },
+        { mode = "i", "<A-p>", function() require("neocodeium").cycle_or_complete(-1) end },
         { mode = "i", "<A-c>", function() require("neocodeium").clear() end },
         -- stylua: ignore end
     },
