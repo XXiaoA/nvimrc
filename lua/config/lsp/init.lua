9756a3361ddebdc0f8cd78b149feb744259ad6b6
XXiaoA <isxxiaoa@gmail.com>
2025-07-18 00:20:38 +0800

    update

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -107,13 +107,45 @@ return {
         end,
     },
 
-    -- {
-    --     "mfussenegger/nvim-lint",
-    --     opts = {
-    --         linters_by_ft = {
-    --             -- lua = { "selene", "luacheck" },
-    --             -- markdown = { "markdownlint" },
-    --         },
-    --     },
-    -- },
+    {
+        "ofseed/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    { "Bilal2453/luvit-meta", lazy = true },
+
+    {
+        "RRethy/vim-illuminate",
+        event = { "VeryLazy", "LspAttach" },
+        config = function()
+            require("illuminate").configure({
+                delay = 200,
+                modes_allowlist = { "n" },
+                large_file_cutoff = 2000,
+                large_file_overrides = {
+                    providers = { "lsp" },
+                },
+            })
+        end,
+        keys = {
+            {
+                "]]",
+                function()
+                    require("illuminate").goto_next_reference()
+                end,
+                desc = "Next Reference",
+            },
+            {
+                "[[",
+                function()
+                    require("illuminate").goto_prev_reference()
+                end,
+                desc = "Prev Reference",
+            },
+        },
+    },
 }
