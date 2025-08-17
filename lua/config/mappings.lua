a37084adb76ca6e4da36bee6ef8ff776570bc1e1
XXiaoA <isxxiaoa@gmail.com>
2025-08-18 01:42:45 +0800

    fix

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -146,7 +146,6 @@ nmap("gcA", function()
 end, { desc = "Comment at the end of line" })
 
 -- remove the default LSP keymaps
-vim.keymap.del("n", "grt")
 vim.keymap.del("n", "grn")
 vim.keymap.del("n", "grr")
 vim.keymap.del("n", "gri")
