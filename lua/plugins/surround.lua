49ad965731bd6ac0643f35e0f621be7eb459e61a
XXiaoA <isxxiaoa@gmail.com>
2026-03-01 20:13:29 +0800

    nvim-surround migrate v3 to v4

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,41 +1,38 @@
 local M = {
     "kylechui/nvim-surround",
     event = "VeryLazy",
+    init = function()
+        vim.g.nvim_surround_no_mappings = true
+    end,
+    config = function()
+        vim.keymap.set("n", "sa", "<Plug>(nvim-surround-normal)", {
+            desc = "Add a surrounding pair around a motion (normal mode)",
+        })
+        vim.keymap.set("n", "ssa", "<Plug>(nvim-surround-normal-cur)", {
+            desc = "Add a surrounding pair around the current line (normal mode)",
+        })
+        vim.keymap.set("n", "sA", "<Plug>(nvim-surround-normal-line)", {
+            desc = "Add a surrounding pair around a motion, on new lines (normal mode)",
+        })
+        vim.keymap.set("n", "ssA", "<Plug>(nvim-surround-normal-cur-line)", {
+            desc = "Add a surrounding pair around the current line, on new lines (normal mode)",
+        })
+        vim.keymap.set("x", "sa", "<Plug>(nvim-surround-visual)", {
+            desc = "Add a surrounding pair around a visual selection",
+        })
+        vim.keymap.set("x", "sA", "<Plug>(nvim-surround-visual-line)", {
+            desc = "Add a surrounding pair around a visual selection, on neq lines",
+        })
+        vim.keymap.set("n", "sd", "<Plug>(nvim-surround-delete)", {
+            desc = "Delete a surrounding pair",
+        })
+        vim.keymap.set("n", "sc", "<Plug>(nvim-surround-change)", {
+            desc = "Change a surrounding pair",
+        })
+        vim.keymap.set("n", "sC", "<Plug>(nvim-surround-change-line)", {
+            desc = "Change a surrounding pair, putting replacements on neq lines",
+        })
+    end,
 }
-
-M.opts = function()
-    local config = require("nvim-surround.config")
-    return {
-        keymaps = {
-            insert = "<C-g>s",
-            insert_line = "<C-g>S",
-            normal = "sa",
-            normal_cur = "ssa",
-            normal_line = "sA",
-            normal_cur_line = "ssA",
-            visual = "sa",
-            visual_line = "sA",
-            delete = "sd",
-            change = "sr",
-            change_line = "sR",
-        },
-        surrounds = {
-            -- remove the default configuration
-            ["i"] = false,
-
-            ["?"] = { -- TODO: Add find/delete/change functions
-                add = function()
-                    local left_delimiter = config.get_input("Enter the left delimiter: ")
-                    local right_delimiter = left_delimiter and config.get_input("Enter the right delimiter: ")
-                    if right_delimiter then
-                        return { { left_delimiter }, { right_delimiter } }
-                    end
-                end,
-                find = function() end,
-                delete = function() end,
-            },
-        },
-    }
-end
 
 return M
